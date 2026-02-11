e244721b63b1d68c6645afae86a2987db9ceb561
ana-maria-ursache <Ana.Ursache@cognizant.com>
2026-02-11 10:00:27 +0200

    fix:css

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div className="mainContainer">
+      <div className="main-container">
         <NavBar />
 
         <Routes>
